9006046a1b5592c773cbd10ab472bafaaa41fb6a
amar-hemmour98 <78423578+amar-hemmour98@users.noreply.github.com>
2021-03-12 23:12:23 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/voicy_v2/activity/AjoutPatientActivity.java
@@ -0,0 +1,50 @@
+package com.example.voicy_v2.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.voicy_v2.R;
+import com.example.voicy_v2.model.Patient;
+import com.example.voicy_v2.model.PatientDbHelper;
+
+public class AjoutPatientActivity extends AppCompatActivity {
+    private Patient patient;
+    public static PatientDbHelper patientDbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout_patient);
+
+        patientDbHelper = new PatientDbHelper(this);
+
+        final TextView idPatient = (TextView) findViewById(R.id.idPatient);
+        final TextView genrePatient = (TextView) findViewById(R.id.genre);
+        final TextView commentairePatient = (TextView) findViewById(R.id.commentaire);
+        final Button btnAjout = (Button) findViewById(R.id.btnAjout);
+
+        btnAjout.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (!((idPatient.getText().toString().isEmpty())&&(genrePatient.getText().toString().isEmpty())&&(commentairePatient.getText().toString().isEmpty()))){
+                    patient = new Patient(idPatient.getText().toString(),genrePatient.getText().toString(),commentairePatient.getText().toString());
+                    if(patientDbHelper.ajoutPatient(patient)){
+                        Toast.makeText(AjoutPatientActivity.this, "patient ajouté",Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(AjoutPatientActivity.this, MainActivity.class);
+                        startActivity(intent);
+
+                    }
+                }
+            }
+        });
+
+    }
+}
